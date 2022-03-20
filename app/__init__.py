f37167526fbab73e5a5f4e41f953687b4f2bef65
Bassel Ghazali <basselghazali@anmut.co.uk>
2022-03-20 11:54:29 +0000

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app.tracker import *
+
+db = SQLAlchemy()
+
+
+def create_app(config_type):
+    app = Flask(__name__)
+    configuration = os.path.join(os.getcwd(), 'config', config_type + '.py')
+    app.config.from_pyfile(configuration)
+    
+    db.init_app(app)
+    app.register_blueprint(main)
+
+    return app
+
