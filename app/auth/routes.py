da36ff82bf57d06a619a787fcf9d28f70d892b2b
Bassel Ghazali <basselghazali@anmut.co.uk>
2022-04-08 16:35:54 +0100

    improved registration route to validate and redirect user after registration

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,15 +1,17 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app.auth.forms import RegistrationForm
 from app.auth import authentication as at
+from app.models import User
 
 
 @at.route('/register', methods=['GET', 'POST'])
 def register_user():
-    name, email = None, None
+
     form = RegistrationForm()
 
-    if request.method == 'POST':
-        name = form.name.data
-        email = form.email.data
+    if form.validate_on_submit():
+        User.create_user(user=form.name.data, email=form.email.data, password=form.password.data)
+        flash("Registration successful!")
+        return redirect(url_for('at.login_user'))
 
-    return render_template('registration.html', form=form, name=name, email=email)
+    return render_template('registration.html', form=form)
